b7af7844ae3ec9721a81a5da776bd44cebc197a4
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-05 08:31:12 +0700

    adding content about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,21 @@
-import Layout from '@/sections/Layout'
-import React from 'react'
-import { HiPencilSquare } from 'react-icons/hi2'
+import Layout from "@/sections/Layout";
+import React from "react";
+import { HiPencilSquare } from "react-icons/hi2";
 
 export default function about() {
   return (
     <>
-    <Layout>
+      <Layout>
         <div className="justify-start left-0 ">
-            <p className="">About Me</p>
+          <h1 className="mt-8 mb-2 font-extrabold text-3xl">About Me</h1>
+          <p className="mb-10 sm:mb-20">
+            Introducing my latest project - a creation born from the knowledge
+            and skills I've acquired. With passion and dedication, I've poured
+            my heart into this endeavor, using everything I've learned to craft
+            something truly remarkable
+          </p>
         </div>
-    </Layout>
+      </Layout>
     </>
-  )
+  );
 }
---
+++ src/pages/contact.tsx
@@ -0,0 +1,15 @@
+import Layout from '@/sections/Layout'
+import React from 'react'
+
+export default function contact() {
+  return (
+
+    <div>
+        <Layout>
+            <p>
+            Contact
+            </p>
+        </Layout>
+    </div>
+  )
+}
