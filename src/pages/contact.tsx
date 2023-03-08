510ce48b25d3347730019b297a353d80bd150875
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-08 17:34:24 +0700

    finishing project

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,7 +6,7 @@ export default function contact() {
   return (
     <>
       <Layout>
-        <div className="justify-start left-0 ">
+        <div className=" left-0 h-screen ">
           <h1 className="mt-8 mb-2 font-extrabold text-3xl">Contact</h1>
           <p className="font-medium mt-1 mb-1 md:mb-3 md:mt-1 text-md   md:text-md ">
             I'm excited to connect with everyone so please don’t hesitate to get
@@ -31,6 +31,28 @@ export default function contact() {
                 href="https://github.com/ridhorayputraa"
               >
                 ridhorayputraa
+              </a>
+            </p>
+
+            <p className="p-3 text-gray-300">
+              LinkedIn -{" "}
+              <a
+                target="_blank"
+                className=" text-gray-200 font-bold border-b-2 "
+                href="https://www.linkedin.com/in/ridho-ray-25aa13236/"
+              >
+                Ridho Ray
+              </a>
+            </p>
+
+            <p className="p-3 text-gray-300">
+              Instagram -{" "}
+              <a
+                target="_blank"
+                className=" text-gray-200 font-bold border-b-2 "
+                href="https://www.instagram.com/dailyfrontend.js/"
+              >
+                dailyfrontend
               </a>
             </p>
           </div>
