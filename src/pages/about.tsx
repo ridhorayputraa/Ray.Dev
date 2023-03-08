99fe370c746fcba4277a3421321bd3a0f5cd01a3
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-08 14:00:33 +0700

    adding paragraph to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,11 +12,8 @@ export default function about() {
           <p className="font-extrabold  text-transparent mt-1 mb-1 md:mb-3 md:mt-1 text-4xl   md:text-3xl bg-clip-text bg-gradient-to-r from-indigo-500 via-pink-500 to-pink-500">
             {Text[0].heading}
           </p>
-          <p className="mb-10 sm:mb-20">
-            Introducing my latest project - a creation born from the knowledge
-            and skills I've acquired. With passion and dedication, I've poured
-            my heart into this endeavor, using everything I've learned to craft
-            something truly remarkable
+          <p className="mb-10 sm:mb-20 w-4/5">
+            {Text[0].paragraphAbout}
           </p>
         </div>
       </Layout>
