bd5a1a0923a94fb8a62a1a623e9fcb58ed349902
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-08 16:24:36 +0700

    addubg about me fetch all data

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,9 +11,17 @@ export default function about() {
           <p className="font-extrabold  text-transparent mt-1 mb-1 md:mb-3 md:mt-1 text-2xl   md:text-3xl bg-clip-text bg-gradient-to-r from-indigo-500 via-pink-500 to-pink-500">
             {Text[0].heading}
           </p>
-          
-          <p className="mb-10 sm:mb-20 w-4/5">{Text[0].paragraphAbout1}
-          </p>
+          <div className="mb-10 mt-3 sm:mb-20 w-4/5">
+            <p>{Text[0].paragraphAbout1}</p>
+            <br />
+            <p>{Text[0].paragraphAbout2}</p>
+            <br />
+            <p>{Text[0].paragraphAbout3}</p>
+            <br />
+            <p>{Text[0].paragraphAbout4}</p>
+            <br />
+            <p>{Text[0].paragraphAbout5}</p>
+          </div>
         </div>
       </Layout>
     </>
